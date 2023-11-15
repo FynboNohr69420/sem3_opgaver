ca80b56380949283c6630ed0ad6e6c5674f5c3a5
FynboNohr69420 <117977003+FynboNohr69420@users.noreply.github.com>
2023-11-15 11:58:54 +0100

    mod13 opgaver

---
+++ Kasper/modul13/Hashing/HashSetChaining.cs
@@ -0,0 +1,140 @@
+using Hashing;
+
+public class HashSetChaining : HashSet
+{
+    private Node[] buckets;
+    private int currentSize;
+
+    private class Node
+    {
+        public Node(Object data, Node next)
+        {
+            this.Data = data;
+            this.Next = next;
+        }
+        public Object Data { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public HashSetChaining(int size)
+    {
+        buckets = new Node[size];
+        currentSize = 0;
+    }
+
+    public bool Contains(Object x)
+    {
+        int h = HashValue(x);
+        Node bucket = buckets[h];
+        bool found = false;
+        while (!found && bucket != null)
+        {
+            if (bucket.Data.Equals(x))
+            {
+                found = true;
+            }
+            else
+            {
+                bucket = bucket.Next;
+            }
+        }
+        return found;
+    }
+
+    public bool Add(Object x)
+    {
+        int h = HashValue(x);
+
+        Node bucket = buckets[h];
+        bool found = false;
+        while (!found && bucket != null)
+        {
+            if (bucket.Data.Equals(x))
+            {
+                found = true;
+            }
+            else
+            {
+                bucket = bucket.Next;
+            }
+        }
+
+        if (!found)
+        {
+            Node newNode = new Node(x, buckets[h]);
+            buckets[h] = newNode;
+            currentSize++;
+        }
+
+        return !found;
+    }
+
+    public bool Remove(Object x)
+    {
+        int h = HashValue(x);
+
+        Node bucket = buckets[h];
+        Node previous = null;
+
+        while (bucket != null)
+        {
+            if (bucket.Data.Equals(x)) // Node to be removed is found
+            {
+                
+                if (previous != null) // If not the head of the linked list
+                {
+                    buckets[h] = bucket.Next;
+                }
+                else // If the head of the linked list
+                {
+                    
+                    buckets[h] = bucket.Next; // Node links to next node, effectively removing it
+                }
+
+                currentSize--;
+                return true; // Object successfully removed
+            }
+
+            previous = bucket;
+            bucket = bucket.Next;
+        }
+
+        return false; // Object not found
+    }
+
+    private int HashValue(Object x)
+    {
+        int h = x.GetHashCode();
+        if (h < 0)
+        {
+            h = -h;
+        }
+        h = h % buckets.Length;
+        return h;
+    }
+
+    public int Size()
+    {
+        return currentSize;
+    }
+
+    public override String ToString()
+    {
+        String result = "";
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            Node temp = buckets[i];
+            if (temp != null)
+            {
+                result += i + "\t";
+                while (temp != null)
+                {
+                    result += temp.Data + " (h:" + HashValue(temp.Data) + ")\t";
+                    temp = temp.Next;
+                }
+                result += "\n";
+            }
+        }
+        return result;
+    }
+}
